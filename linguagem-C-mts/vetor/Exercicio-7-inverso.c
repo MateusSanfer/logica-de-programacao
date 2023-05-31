1bc6811fca02361e59757a70e0ac73786e7fcefb
mateussanfer <126841158+MateusSanfer@users.noreply.github.com>
2023-05-31 08:47:27 -0300

    Add files via upload

--- linguagem-C-mts/vetor/Exercicio-7-inverso.c
+++ linguagem-C-mts/vetor/Exercicio-7-inverso.c
@@ -4,24 +4,25 @@
 int main(){
 
     int i, valores[6];
-
+ 
+    while (valores[i] % 2 == 0)
+        {
     for ( i = 0; i < 6; i++)
     {
-        printf("Digite o %d numero: \n", i + 1);
+        printf("Digite o %d numero: \n",i);
         scanf("%d", &valores[i]);
 
-        while (valores[i] % 2 == 0)
-        {
-            printf("Digite um numero: \n");
-            scanf("%d", &valores[i]);
         }
     
     }
+    fflush(stdin);
 
-    for ( i = 6; i < 0; i--)
-    {
-        printf("Valores: \n", valores[i]);
+    for ( i = 5; i >= 0; i--)
+    { 
+
+        printf("Valores: %d\n", valores[i]);
+    
     }
     
-    
+    return 0;
 }
